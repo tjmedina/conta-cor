ca7135c627593240a59466bcd8cc3271018fe205
Trino <quintanatrinos@gmail.com>
2023-07-21 08:57:14 -0400

    modified:   .gitignore 	new file:   conta-corV1.py

--- colorv1.py
+++ colorv1.py
@@ -33,9 +33,45 @@ tiempo_desaparicion = parametros["tiempo_desaparicion"]
 resolucion_captura = tuple(resolucion_captura)
 posiciones_areas = [tuple(pos) for pos in posiciones_areas]
 
-cap = cv2.VideoCapture(3)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, resolucion_captura[0])
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resolucion_captura[1])
+
+cam = 3             # Camera
+width = 640         # Largura
+height = 480        # Altura
+fps = 60            # FPS 25/30/50/60
+
+codec = 0x47504A4D  # MJPG
+brightness = 55     # Brilho
+contrast = 130      # Contraste
+saturation = 112    # Saturação
+
+# Câmera Hikvision não tem essas funçoes
+focus = 0           # Foco
+sharpness = 0       # Nitidez
+exposure = 0        # Exposição
+
+
+# FPS Teste
+start_time = time.time()
+display_time = 10
+fc = 0
+p_fps = 0
+
+# Conexão da câmera
+camera = cv2.VideoCapture(cam)
+
+# Configuração da câmera
+camera.set(cv2.CAP_PROP_FOURCC, codec)
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+camera.set(cv2.CAP_PROP_FPS, fps)
+camera.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+camera.set(cv2.CAP_PROP_CONTRAST, contrast)
+camera.set(cv2.CAP_PROP_SATURATION, saturation)
+
+#camera.set(cv2.CAP_PROP_FOCUS, focus)
+#camera.set(cv2.CAP_PROP_SHARPNESS, sharpness)
+# camera.set(cv2.CAP_PROP_EXPOSURE, exposure)
 
 colores_detectados = {}
 total_colores_detectados = {}
@@ -50,67 +86,79 @@ for color in rango_colores.keys():
         tiempo_actual[color] = None
 
 while True:
-    ret, frame = cap.read()
-    if ret:
-        frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
-        # Voltear la imagen horizontalmente (modo espejo)
-        #frame = cv2.flip(frame, 1)
-
-        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    ret, frame = camera.read()
+    if not ret:
+        break
+    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    fc+=1
+    
+    TIME = time.time() - start_time
         
-        height, width, _ = frame.shape
+    if (TIME) >= display_time :
+        p_fps = fc / (TIME)
+        fc = 0
+        start_time = time.time()
         
-        for color in rango_colores.keys():
-            if color != "Undefined":
-                colores_detectados[color] = 0
+        fps_disp = "FPS: "+str(p_fps)[:5]
 
-        for i in range(cantidad_areas):
-            area_x, area_y = posiciones_areas[i]
-            centroid_x = area_x + int(ancho_area / 2)
-            centroid_y = area_y + int(alto_area / 2)
+    # Voltear la imagen horizontalmente (modo espejo)
+    #frame = cv2.flip(frame, 1)
 
-            pixel_center = hsv_frame[centroid_y, centroid_x]
-            hue_value = pixel_center[0]
-            saturation_value = pixel_center[1]
-            value_value = pixel_center[2]
-            color = determine_color(hue_value, saturation_value, value_value, rango_colores)
-            #print(pixel_center[0], pixel_center[1], pixel_center[2])
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+    height, width, _ = frame.shape
+        
+    for color in rango_colores.keys():
+        if color != "Undefined":
+            colores_detectados[color] = 0
+
+    for i in range(cantidad_areas):
+        area_x, area_y = posiciones_areas[i]
+        centroid_x = area_x + int(ancho_area / 2)
+        centroid_y = area_y + int(alto_area / 2)
+
+        pixel_center = hsv_frame[centroid_y, centroid_x]
+        hue_value = pixel_center[0]
+        saturation_value = pixel_center[1]
+        value_value = pixel_center[2]
+        color = determine_color(hue_value, saturation_value, value_value, rango_colores)
+        #print(pixel_center[0], pixel_center[1], pixel_center[2])
 
             
             
-            cv2.circle(frame, (centroid_x, centroid_y), 1, (255, 255, 255), 2)
+        cv2.circle(frame, (centroid_x, centroid_y), 1, (255, 255, 255), 2)
             
 
-            if color != "Undefined":
-                cv2.rectangle(frame, (area_x, area_y), (area_x + ancho_area, area_y + alto_area), (255, 255, 255), 2)
-                #cv2.putText(frame, color, (area_x, area_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                cv2.putText(frame, color, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                if ultimo_frame_detectado[color] is None:
+        if color != "Undefined":
+            #cv2.rectangle(frame, (area_x, area_y), (area_x + ancho_area, area_y + alto_area), (255, 255, 255), 2)
+            cv2.putText(frame, color, (area_x, area_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.putText(frame, color, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            if ultimo_frame_detectado[color] is None:
+                ultimo_frame_detectado[color] = frame.copy()
+                tiempo_actual[color] = time.time()
+                colores_detectados[color] += 1
+            else:
+                tiempo_transcurrido = time.time() - tiempo_actual[color]
+                if tiempo_transcurrido >= tiempo_desaparicion:
                     ultimo_frame_detectado[color] = frame.copy()
                     tiempo_actual[color] = time.time()
                     colores_detectados[color] += 1
-                else:
-                    tiempo_transcurrido = time.time() - tiempo_actual[color]
-                    if tiempo_transcurrido >= tiempo_desaparicion:
-                        ultimo_frame_detectado[color] = frame.copy()
-                        tiempo_actual[color] = time.time()
-                        colores_detectados[color] += 1
 
-        for color, count in colores_detectados.items():
-            total_colores_detectados[color] += count
+    for color, count in colores_detectados.items():
+        total_colores_detectados[color] += count
 
-        total_acumulado = sum(total_colores_detectados.values())
-        text_y = 360
-        for color, count in total_colores_detectados.items():
-            cv2.putText(frame, color + ": " + str(count), (7, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            text_y += 20
+    total_acumulado = sum(total_colores_detectados.values())
+    text_y = 360
+    for color, count in total_colores_detectados.items():
+        cv2.putText(frame, color + ": " + str(count), (7, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        text_y += 20
+        #print(color + ": " + str(count))
+    cv2.putText(frame, "Total: " + str(total_acumulado), (7, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        
+    print("Total: " + str(total_acumulado))
+    cv2.imshow("Contagems", frame)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        break
 
-        cv2.putText(frame, "Total: " + str(total_acumulado), (7, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-        cv2.imshow("Contagems", frame)
-        if cv2.waitKey(1) & 0xFF == ord('s'):
-            break
-
-cap.release()
+camera.release()
 cv2.destroyAllWindows()
---
+++ conta-corV1.py
@@ -0,0 +1,171 @@
+import cv2
+import numpy as np
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Función para dibujar contornos y calcular centroides dentro del ROI
+def dibujar(frame, mask, color, roi_vertices):
+    contornos, _ = cv2.findContours(
+        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    centroids_in_roi = []
+
+    for c in contornos:
+        area = cv2.contourArea(c)
+        if area > 3000:
+            M = cv2.moments(c)
+            if M["m00"] == 0:
+                M["m00"] = 1
+            x = int(M["m10"] / M["m00"])
+            y = int(M['m01'] / M['m00'])
+
+            if cv2.pointPolygonTest(roi_vertices, (x, y), False) == 1:
+                centroids_in_roi.append((x, y))
+
+            nuevoContorno = cv2.convexHull(c)
+            cv2.circle(frame, (x, y), 3, color, -1)
+            cv2.putText(frame, '{},{}'.format(x, y), (x + 10, y),
+                        font, 0.75, color, 1, cv2.LINE_AA)
+            cv2.drawContours(frame, [nuevoContorno], 0, color, 3)
+
+    return centroids_in_roi
+
+# Función vacía para la creación de trackbars
+def empty(a):
+    pass
+
+# Función para ajustar el rango de color mediante trackbars
+def configurar_rango_color():
+    cv2.namedWindow("HSV")
+    cv2.resizeWindow("HSV", 640, 240)
+    cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
+    cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)
+    cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
+    cv2.createTrackbar("HUE Max", "HSV", 179, 179, empty)
+    cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
+    cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
+
+
+def main():
+    # Configurar el rango de color mediante trackbars
+    configurar_rango_color()
+
+    # Configuración de la cámara
+    cam = 5
+    width, height = 640, 480
+    fps = 60
+    brightness, contrast, saturation = 55, 130, 112
+
+    cap = cv2.VideoCapture(cam)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cap.set(cv2.CAP_PROP_FPS, fps)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+    cap.set(cv2.CAP_PROP_CONTRAST, contrast)
+    cap.set(cv2.CAP_PROP_SATURATION, saturation)
+
+    azulBajo = np.array([90, 11, 189], np.uint8)
+    azulAlto = np.array([105, 255, 255], np.uint8)
+
+    amarilloBajo = np.array([18, 12, 157], np.uint8)
+    amarilloAlto = np.array([32, 255, 255], np.uint8)
+
+    red_low = np.array([0, 76, 171], np.uint8)
+    red_high = np.array([10, 255, 255], np.uint8)
+
+    # Definir las coordenadas de los vértices del polígono ROI
+    roi_vertices = np.array([[430, 15],
+                             [430, 474],
+                             [435, 474],
+                             [435, 15]], np.int32)
+
+    # Initialize counters and lists to keep track of individual detections
+    blue_detections = []
+    yellow_detections = []
+    red_detections = []
+
+    while True:
+        ret, frame = cap.read()
+
+        # Verificar si se capturó correctamente un cuadro
+        if not ret:
+            break
+
+        frameHsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        h_min = cv2.getTrackbarPos("HUE Min", "HSV")
+        h_max = cv2.getTrackbarPos("HUE Max", "HSV")
+        s_min = cv2.getTrackbarPos("SAT Min", "HSV")
+        s_max = cv2.getTrackbarPos("SAT Max", "HSV")
+        v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
+        v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+
+        lower = np.array([h_min, s_min, v_min])
+        upper = np.array([h_max, s_max, v_max])
+        mask = cv2.inRange(frameHsv, lower, upper)
+        # print(lower)
+
+        maskAzul = cv2.inRange(frameHsv, azulBajo, azulAlto)
+        maskAmarillo = cv2.inRange(frameHsv, amarilloBajo, amarilloAlto)
+        maskRed = cv2.inRange(frameHsv, red_low, red_high)
+
+        # Llamar a la función dibujar() con el argumento frame
+        centroids_azul = dibujar(frame, maskAzul, (255, 0, 0), roi_vertices)
+        centroids_amarillo = dibujar(
+            frame, maskAmarillo, (0, 255, 255), roi_vertices)
+        centroids_rojo = dibujar(frame, maskRed, (0, 0, 255), roi_vertices)
+
+        # Draw the ROI polygon on the frame
+        cv2.polylines(frame, [roi_vertices], isClosed=False,
+                      color=(0, 255, 0), thickness=2)
+
+        cv2.circle(frame, (30, 32), 15, (255, 0, 0), -1)
+        cv2.circle(frame, (30, 72), 15, (0, 255, 255), -1)
+        cv2.circle(frame, (30, 112), 15, (0, 0, 255), -1)
+
+        # Update counters based on the number of detected objects within ROI
+        blue_detected = len(centroids_azul)
+        yellow_detected = len(centroids_amarillo)
+        red_detected = len(centroids_rojo)
+
+        # Add individual detections to the corresponding lists
+        blue_detections.append(blue_detected)
+        yellow_detections.append(yellow_detected)
+        red_detections.append(red_detected)
+
+        # Calculate the total count for each color by summing all detections
+        total_blue = sum(blue_detections)
+        total_yellow = sum(yellow_detections)
+        total_red = sum(red_detections)
+
+        # Draw the total counts on the frame for each color
+        cv2.putText(frame, f': {total_blue}', (50, 40),
+                    font, 0.75, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, f': {total_yellow}',
+                    (50, 80), font, 0.75, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, f': {total_red}',
+                    (50, 120), font, 0.75, (0, 0, 0), 2, cv2.LINE_AA)
+        total = total_blue + total_yellow + total_red
+        cv2.putText(frame, f'Total: {total}', (15, 160),
+                    font, 0.75, (0, 0, 0), 2, cv2.LINE_AA)
+
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        hStack = np.hstack([frame, mask, result])
+        # cv2.imshow('Original', frame)
+        # cv2.imshow('HSV Color Space', iframeHsv)
+        # cv2.imshow('Mask', mask)
+        # cv2.imshow('Result', result)
+        cv2.imshow('Horizontal Stacking', hStack)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Liberar la cámara y cerrar todas las ventanas de OpenCV al finalizar
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
